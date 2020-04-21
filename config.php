627d87c33d5b6e3ca55a203ce235038faaad3b75
MaksSlisar <maksimslisarcuk@gmail.com>
2020-04-21 13:18:42 +0300

    change db accsess

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 
 <?php
-$host = "localhost";
-$db = "messages";
+$host = "eu-mm-auto-dub-03-b.cleardb.net";
+$db = "heroku_8f723ab4158d2db";
 $charset = "UTF8";
-$user = "root";
-$pass ="";
+$user = "b538e43d6bc357";
+$pass ="85a26568";
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $opt = array(
 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
